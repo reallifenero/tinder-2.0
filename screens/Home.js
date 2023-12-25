e179173c4fb4d04bbac85ba5bd9ca763ec2aa782
NERŌ <reallifenero@gmail.com>
2023-12-25 08:22:07 -0500

    swiper component

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,109 @@
 import React from "react";
 import tw from "tailwind-react-native-classnames";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import Swiper from "react-native-deck-swiper";
 import { useNavigation } from "@react-navigation/core";
+import { Ionicons, AntDesign, Entypo } from "@expo/vector-icons";
 
 import useAuth from "../hooks/useAuth";
+import DUMMY_DATA from "../utils/data";
 
 const Home = () => {
-  const { signOutWithGoogle } = useAuth();
+  const { signOutWithGoogle, user } = useAuth();
   const navigation = useNavigation();
-
+  // console.log(user.photoURL);
   return (
-    <View style={tw`p-5 justify-center items-center h-full`}>
-      <Text>I am the Home Screen</Text>
-      <Button
-        title="Go to chat screen"
-        onPress={() => navigation.navigate("Chat")}
-      />
-      <Button title="Logout" onPress={() => signOutWithGoogle()} />
-    </View>
+    <SafeAreaView>
+      {/* Header */}
+      <View style={tw`items-center relative flex`}>
+        <TouchableOpacity
+          style={tw`absolute left-5`}
+          onPress={() => signOutWithGoogle()}
+        >
+          <Image
+            source={{ uri: user.photoURL }}
+            style={{ height: 30, width: 30, borderRadius: 100 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require("../assets/icon.png")}
+            style={{ height: 40, width: 40, borderRadius: 100 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`absolute right-5`}
+          onPress={() => navigation.navigate("Chat")}
+        >
+          <Ionicons name="chatbubbles-sharp" size={32} color="black" />
+        </TouchableOpacity>
+      </View>
+      <Swiper
+        cards={[...DUMMY_DATA]}
+        renderCard={(card) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text}>{card}</Text>
+            </View>
+          );
+        }}
+        onSwiped={(cardIndex) => {
+          console.log(cardIndex);
+        }}
+        onSwipedAll={() => {
+          console.log("onSwipedAll");
+        }}
+        cardIndex={0}
+        backgroundColor={"#4FD0E9"}
+        stackSize={3}
+      >
+        <Button
+          onPress={() => {
+            console.log("oulala");
+          }}
+          title="Press me"
+        >
+          You can press me
+        </Button>
+      </Swiper>
+      <View style={tw`p-5 justify-start items-center h-full`}>
+        <Text>I am the Home Screen</Text>
+        <Button
+          title="Go to chat screen"
+          onPress={() => navigation.navigate("Chat")}
+        />
+        <Button title="Logout" onPress={() => signOutWithGoogle()} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF",
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
+  },
+});
