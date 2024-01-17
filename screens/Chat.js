19bddf1e5e0732cde1dd0141d34e275eed806c9d
NERŌ <reallifenero@gmail.com>
2024-01-16 21:45:43 -0500

    chatlist component

--- screens/Chat.js
+++ screens/Chat.js
@@ -2,11 +2,13 @@ import { SafeAreaView, Text } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import Header from "../components/Header";
+import ChatList from "../components/ChatList";
 
 const Chat = () => {
   return (
     <SafeAreaView>
       <Header title="Chat" />
+      <ChatList />
     </SafeAreaView>
   );
 };
